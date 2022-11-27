a440ed78e632e11fa72ebe84f10639ebca55a973
AFURO-1130 <abcshotaro616@gmail.com>
2022-11-28 08:59:40 +0900

    fix

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,11 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-const program = require('commander')
 
-program
-  .usage('-n yourname')
-  .option('-n, --name <value>', 'your name', String, 'World')
-  .parse(process.argv)
 
-console.log('Hello ' + program.name + '!')
+console.log('Hello ' + "kinjyo" + '!')
